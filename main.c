79c7a3d30ea4d219a7b0c50fc93f3ac22b50084f
Denis <Kepel@localhost.localdomain>
2018-04-19 20:34:25 +0700

    Fix

--- main.c
+++ main.c
@@ -29,10 +29,12 @@ void print_array(int *arr, int size)
 int main()
 {
     int arr[] = {3, 1, 4, 1, 5, 9, 2};
+    
 
     const int min = min_element(arr, N_ELEMENTS(arr));
-
     print_array(arr, N_ELEMENTS(arr));
+
+    
     printf("Min element: %d\n", min);
 
     return 0;
